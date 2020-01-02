006524b003ee69f5eefcabec8ec83fd41267b365
chenshan <869395614@qq.com>
2020-01-02 10:29:14 +0800

    修改了router.js

---
+++ src/router.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-31 17:23:57
+ * @LastEditTime : 2020-01-02 10:28:28
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \myprojects\src\router.js
+ */
+
