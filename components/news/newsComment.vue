dd2568e110af4774a57e52d2cc13631cb14a7ba2
chenshan <869395614@qq.com>
2020-01-08 16:34:01 +0800

    添加评论子组件，并父组件向子组件传值

---
+++ components/news/newsComment.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-01-08 11:23:30
+ * @LastEditTime : 2020-01-08 16:32:27
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \myprojects\components\news\newsComment.vue
+ -->
+<template>
+    <div>
+        这是comment评论区域----{{this.id}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+       
+    },
+    props:['id']
+}
+</script>
+
+<style lang="scss">
+    
+</style>
--- components/news/newsDetail.vue
+++ components/news/newsDetail.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-01-07 16:39:17
- * @LastEditTime : 2020-01-07 17:48:04
+ * @LastEditTime : 2020-01-08 16:04:00
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \myprojects\components\news\newsDetail.vue
@@ -9,11 +9,25 @@
 <template>
     <div>
         detail---{{this.$route.params}}
+        <newsComment :id="this.id"></newsComment>
     </div>
+
 </template>
 <script>
+import newsComment from './newsComment.vue'
 export default {
-    
+    data(){
+        return {
+            id:this.$route.params
+        }
+    },
+    methods:{
+
+    },
+    components:{
+        newsComment:newsComment
+    }
+
 }
 </script>
 <style lang="scss">
